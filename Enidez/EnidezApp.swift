41d8b9e04ec233fb6cf8e61c32e4b6a4d1ded792
Enidez <azzedinebenbahlouli@gmail.com>
2026-08-28 16:20:18 +0200

    Initial Commit

---
+++ Enidez/EnidezApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnidezApp.swift
+//  Enidez
+//
+//  Created by Enidez on 28/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnidezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
